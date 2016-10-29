9e31d0451d3b20203aa5e95b02a046a265eb85cb
AprilHickson <Aprilhicksontagg@gmail.com>
2016-10-29 17:58:04 +1300

    Added Json Items to the PHP Json file Manually

--- data/images.php
+++ data/images.php
@@ -1,4 +1,8 @@
 {"images":[
     {"Path":"data\\images\\Caravan.jpg", "Notes":"Outside of Dad's new Caravan", "Tags":["Caravan", "Outside"]},
-    {"Path":"data\\images\\Sue-In-Caravan.jpg", "Notes":"Picture of Sue in Dad's new Caravan", "Tags":["Caravan", "Inside", "Person"]}
+    {"Path":"data\\images\\Sue-In-Caravan.jpg", "Notes":"Picture of Sue in Dad's new Caravan", "Tags":["Caravan", "Inside", "Person"]},
+    {"Path":"data\\images\\Taylor-Swift-On-Grass.jpg", "Notes":"Taylor Swift sitting on grass", "Tags":["Taylor Swift", "Grass", "Sitting"]},
+    {"Path":"data\\images\\Taylor-Swift-On-Stage-Bending-Forward.jpg", "Notes":"Taylor Swift on stage bending forward", "Tags":["Taylor Swift", "Stage", "Concert"]},
+    {"Path":"data\\images\\Taylor-Swift-On-Stairs.jpg", "Notes":"Taylor Swift sitting on stairs", "Tags":["Taylor Swift", "Stairs", "Marble"]},
+    {"Path":"data\\images\\Taylor-Swift-With-Black-Background.jpg", "Notes":"Taylor Swift posing on a black background", "Tags":["Taylor Swift", "Posing", "Black Dress"]}
 ]}
